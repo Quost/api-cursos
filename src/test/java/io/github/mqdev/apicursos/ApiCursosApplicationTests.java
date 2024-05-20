c29b9e12425115c9714bab65a949d426d7ea2a61
Matheus Quost <matheusquost@gmail.com>
2024-05-20 10:50:03 -0300

    chore: Add initial project files and configuration

---
+++ src/test/java/io/github/mqdev/apicursos/ApiCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mqdev.apicursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
