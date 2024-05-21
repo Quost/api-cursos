ede4d874a7892e3ca5bd07d14374708b6c92355d
Matheus Quost <matheusquost@gmail.com>
2024-05-21 19:44:28 -0300

    feat: Add springdoc-openapi dependency and configure OpenAPI definition

--- src/main/java/io/github/mqdev/apicursos/ApiCursosApplication.java
+++ src/main/java/io/github/mqdev/apicursos/ApiCursosApplication.java
@@ -3,7 +3,17 @@ package io.github.mqdev.apicursos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "API de Cursos",
+		version = "1.0",
+		description = "API para cadastro de cursos"
+	)
+)
 public class ApiCursosApplication {
 
 	public static void main(String[] args) {
