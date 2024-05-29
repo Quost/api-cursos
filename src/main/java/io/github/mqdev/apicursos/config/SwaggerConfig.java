3a8f0ead1e5ffcfff0091f82f9a19b02725dc6cd
Matheus Quost <matheusquost@gmail.com>
2024-05-29 14:22:27 -0300

    chore: Add Swagger configuration for API documentation

---
+++ src/main/java/io/github/mqdev/apicursos/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package io.github.mqdev.apicursos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gestão de Cursos")
+                        .version("1.0")
+                        .description("API CRUD de cursos"))
+                        .schemaRequirement("jwt_auth", securityScheme());
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .name("jwt_auth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
+    }
+}
