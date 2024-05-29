9243f5dc1c4305b6d6c31ed3c980660ae42abdaf
Matheus Quost <matheusquost@gmail.com>
2024-05-29 15:17:55 -0300

    fix: Add @Service annotation to CreateUserUseCase

--- src/main/java/io/github/mqdev/apicursos/modules/user/useCases/CreateUserUseCase.java
+++ src/main/java/io/github/mqdev/apicursos/modules/user/useCases/CreateUserUseCase.java
@@ -2,11 +2,13 @@ package io.github.mqdev.apicursos.modules.user.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import io.github.mqdev.apicursos.exceptions.UserAlreadyExistsException;
 import io.github.mqdev.apicursos.modules.user.entities.UserEntity;
 import io.github.mqdev.apicursos.modules.user.repositories.UserRepository;
 
+@Service
 public class CreateUserUseCase {
 
     @Autowired
